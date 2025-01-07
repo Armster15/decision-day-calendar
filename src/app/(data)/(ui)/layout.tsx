1ef05c31a54372cfa810e1e182c59543fd0a7940
Armaan A <armaanaggarwal@gmail.com>
2025-01-06 23:53:34 -0800

    update padding

--- src/app/(data)/(ui)/layout.tsx
+++ src/app/(data)/(ui)/layout.tsx
@@ -41,7 +41,7 @@ export default function UILayout({ children }: PropsWithChildren) {
         </div>
       </nav>
 
-      <div className="px-8 pt-4 pb-36">
+      <div className="p-4 sm:px-8 pb-36">
         {isReady ? children : <></>}
         <noscript>
           <p>Please enable JavaScript</p>
