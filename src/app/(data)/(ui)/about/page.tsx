77d7a67a4a9b4c4afdc38d8c63338ae9701affc5
Armaan A <armaanaggarwal@gmail.com>
2025-01-06 23:18:07 -0800

    fix build errors

--- src/app/(data)/(ui)/about/page.tsx
+++ src/app/(data)/(ui)/about/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useActionState, useContext } from "react";
-
 export default function Advanced() {
   return (
     <main className="space-y-4">
@@ -9,19 +7,21 @@ export default function Advanced() {
         <p>Why use this?</p>
         <ul className="list-disc ml-8">
           <li>
-            Most data for colleges is already here so minimal work is required:
-            just select what colleges you want to follow
+            {
+              "Most data for colleges is already here so minimal work is required: just select what colleges you want to follow"
+            }
           </li>
-          <li>Flexible: if a college isn't here, simply add it</li>
+          <li>{"Flexible: if a college isn't here, simply add it"}</li>
           <li>
-            Up to date: college decision days are automatically updated when new
-            information comes out
+            {
+              "Up to date: college decision days are automatically updated when new information comes out"
+            }
           </li>
         </ul>
       </div>
 
       <p>
-        {"We're"} open source! Visit{" "}
+        {"We're open source! Visit "}
         <a
           className="link"
           href="https://github.com/Armster15/decision-day-calendar"
@@ -32,7 +32,7 @@ export default function Advanced() {
       </p>
 
       <p>
-        Created by{" "}
+        {"Created by "}
         <a className="link" href="https://armaan.cc" target="_blank">
           armaan
         </a>
