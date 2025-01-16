250a2483e7306dc20685fd22bb74e1f29dedf2fd
Armaan A <armaanaggarwal@gmail.com>
2025-01-15 18:47:06 -0800

    use a lib to humanize stuff

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,17 +1,5 @@
-import {
-  addDays,
-  addHours,
-  addMinutes,
-  addMonths,
-  addYears,
-  differenceInYears,
-  differenceInMonths,
-  differenceInDays,
-  differenceInHours,
-  differenceInMinutes,
-  differenceInSeconds,
-  format as formatDate,
-} from "date-fns";
+import { format as formatDate } from "date-fns/format";
+import humanizeDuration from "humanize-duration";
 
 export function formatCollegeDecisionDate(date: Date) {
   let formatStr = "EEE MMM dd, yyyy";
@@ -42,49 +30,28 @@ export function dateHasTime(date: Date) {
   );
 }
 
-export function pluralize(singular: string, plural: string, number: number) {
-  return number === 1 ? singular : plural;
-}
-
-export function getFormattedDifference(date1: Date, date2: Date) {
-  const start = new Date(date1);
-  const end = new Date(date2);
-
-  const years = differenceInYears(end, start);
-  const dateAfterYears = addYears(start, years);
-
-  const months = differenceInMonths(end, dateAfterYears);
-  const dateAfterMonths = addMonths(dateAfterYears, months);
-
-  const days = differenceInDays(end, dateAfterMonths);
-  const dateAfterDays = addDays(dateAfterMonths, days);
-
-  const hours = differenceInHours(end, dateAfterDays);
-  const dateAfterHours = addHours(dateAfterDays, hours);
-
-  const minutes = differenceInMinutes(end, dateAfterHours);
-  const dateAfterMinutes = addMinutes(dateAfterHours, minutes);
-
-  const seconds = differenceInSeconds(end, dateAfterMinutes);
-
-  // Build the result string
-  const parts = [];
-  if (years > 0) parts.push(`${years} ${pluralize("yr", "yrs", years)}`);
-  if (months > 0)
-    parts.push(`${months} ${pluralize("month", "months", months)}`);
-  if (days > 0) parts.push(`${days} ${pluralize("day", "days", days)}`);
-  if (hours > 0) parts.push(`${hours} ${pluralize("hr", "hrs", hours)}`);
-  if (minutes > 0)
-    parts.push(`${minutes} ${pluralize("min", "mins", minutes)}`);
-  if (seconds > 0)
-    parts.push(`${seconds} ${pluralize("sec", "secs", seconds)}`);
-
-  return parts.join(" ");
-}
-
 function getTimeZoneAbbreviation(date = new Date()) {
   const formatter = new Intl.DateTimeFormat("en-US", { timeZoneName: "short" });
   const parts = formatter.formatToParts(date);
   const timeZonePart = parts.find((part) => part.type === "timeZoneName");
   return timeZonePart ? timeZonePart.value : null;
+}
+
+const shortEnglishHumanizer = humanizeDuration.humanizer({
+  language: "shortEn",
+  round: true,
+  units: ["mo", "d", "h", "m", "s"],
+  languages: {
+    shortEn: {
+      mo: (count) => (count === 1 ? "month" : "months"),
+      d: (count) => (count === 1 ? "day" : "days"),
+      h: (count) => (count === 1 ? "hour" : "hours"),
+      m: (count) => (count === 1 ? "minute" : "minutes"),
+      s: (count) => (count === 1 ? "second" : "seconds"),
+    },
+  },
+});
+
+export function getFormattedDifference(start: Date, end: Date) {
+  return shortEnglishHumanizer(+end - +start);
 }
